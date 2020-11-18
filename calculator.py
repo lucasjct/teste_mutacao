d06a1bc107f971765e13c12fd7ce0829bf835498
Lucas Teixeira <38733166+lucasjct@users.noreply.github.com>
2020-11-17 23:46:05 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,36 +1,23 @@
-from numbers import Number
-
 def soma(x,y):
-    """AOR."""
     return x + y
 
+def multiplicacao(x, y):
+    return x * y
 
-def invert(x):
-    """AOD - DELETE OPERATOR
-       AOR - MUDA OPERATOR"""
-    return -x
+def comparador(val1, val2):
+    if val1 > val2:
+        return True
+    else:
+        return False
 
-
-def sum_(*vals):
-    """
-    ASR - Troca o operador de atribuicao
-    CRP - Troca as constantes
-    """
+def iterador(x):
     results = 0
-    for val in vals:
-        results += val
+    for value in range(x):
+        results = results + value
     return results
 
-
-def is_number(val):
-    """ 
-    COD - Deletar operadores sozinho (not)
-    COI - Insere um operador lógico
-    """
-    if not isinstance(val, Number):
-        return False
-    return True
-
-def check_number(x,y):
-    if is_number(x) and is_number(y):
-        return True
+def par_impar(num):
+    if num % 2 == 1:
+        return "ímpar"
+    else:
+        return "par"
